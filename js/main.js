ac0d507378f3537afd7340053262478c86f5ed21
fenq <fenq@ukr.net>
2020-02-10 16:14:59 +0200

    First

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.sandwich').click(function () {
+    $('.main-nav__list').slideToggle(500);
+});
+
+const sandwichToggle = function () {
+    // Выбираем элементы, которые нам нужны. В примере мы ищем элементы с классом "sandwich"
+    let sandwichElements = document.querySelectorAll('.sandwich');
+    // Проходим циклом по всем эдементам и на каждый элемент вешаем слушателя, который по клику будет переключать класс
+    sandwichElements.forEach((item) => {
+        item.addEventListener('click', showSandwichTarget);
+    });
+
+    function showSandwichTarget() {
+        this.classList.toggle('is-active');
+    }
+};
+sandwichToggle();
